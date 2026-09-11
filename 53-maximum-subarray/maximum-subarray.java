0e9043ab8f99b9b3820e85258458da8700eab326
Arpit Shukla <167569808+arpitshukla9@users.noreply.github.com>
2026-09-11 13:40:33 +0530

    Time: 1 ms (99.92%) | Memory: 77.3 MB (48.82%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] arr) {
+        int maxi = Integer.MIN_VALUE;
+        int prefix = 0;
+        for(int i = 0; i<arr.length; i++){
+            prefix += arr[i];
+            maxi = Math.max(maxi, prefix);
+            if(prefix < 0) prefix = 0;
+        }
+        return maxi;
+    }
+}
