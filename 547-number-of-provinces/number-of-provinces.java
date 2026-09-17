ca008a36b3a611263c25c7a7b361ca2b6d38c98f
Arpit Shukla <167569808+arpitshukla9@users.noreply.github.com>
2026-09-17 14:18:17 +0530

    Time: 1 ms (85.59%) | Memory: 47.3 MB (43.50%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,29 +1,38 @@
 class Solution {
-    void bfs(int i, boolean[] visit, int[][] connected, int n){
-        // bfs (i) mark it true
+    // void bfs(int i, boolean[] visit, int[][] connected, int n){
+    //     // bfs (i) mark it true
+    //     visit[i] = true;
+    //     // Create a Queue
+    //     Queue<Integer> q = new LinkedList<>();
+    //     // add i first
+    //     q.add(i);
+    //     while(q.size() > 0){
+    //         int front = q.remove();
+    //         for(int j = 0; j < n; j++){
+    //             if(connected[front][j] == 1 && !visit[j]) {
+    //                 q.add(j);
+    //                 visit[j] = true;
+    //             }
+    //         }
+    // }
+
+    void dfs(int i, boolean[] visit, int[][] connected){
+        int n = connected.length;
         visit[i] = true;
-        // Create a Queue
-        Queue<Integer> q = new LinkedList<>();
-        // add i first
-        q.add(i);
-        while(q.size() > 0){
-            int front = q.remove();
             for(int j = 0; j < n; j++){
-                if(connected[front][j] == 1 && !visit[j]) {
-                    q.add(j);
-                    visit[j] = true;
+                if(connected[i][j] == 1 && !visit[j]) {
+                    dfs(j, visit, connected);
                 }
             }
-        }
-
     }
+
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         int count = 0;
-        boolean visited[] = new boolean [n];
-        for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                bfs(i, visited, isConnected, n);
+        boolean visited[] = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, visited, isConnected);
                 count++;
             }
         }
