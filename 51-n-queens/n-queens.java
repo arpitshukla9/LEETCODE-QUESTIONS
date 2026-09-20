ad5587dfcf3a9165ba452684d3242cc9b9cc7fe3
Arpit Shukla <167569808+arpitshukla9@users.noreply.github.com>
2026-09-20 15:14:20 +0530

    Time: 2 ms (81.88%) | Memory: 46.8 MB (28.19%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,69 +1,66 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> ans = new ArrayList<>();
-        char[][] chessBoard = new char[n][n];
-        // initialize board
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                chessBoard[i][j] = '.';
-            }
-        }
+        char[][] board = new char[n][n];
 
-        nQueens(0, chessBoard, ans);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+
+        nQueens(0, board, ans);
         return ans;
     }
-    static void nQueens(int row, char[][] chessBoard, List<List<String>> ans) {
-        int n = chessBoard.length;
-        if (row == n) { // valid answer
+
+    private void nQueens(int row, char[][] board, List<List<String>> ans) {
+        if (row == board.length) {
             List<String> list = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                StringBuilder sb = new StringBuilder();
-                for (int j = 0; j < n; j++) {
-                    sb.append(chessBoard[i][j]);
-                }
-                list.add(sb.toString());
-            }
+
+            for (char[] r : board)
+                list.add(new String(r));
+
             ans.add(list);
             return;
         }
-        // try every column in current row
-        for (int col = 0; col < n; col++) {
-            if (canQueenBePlaced(row, col, chessBoard)) {
-                chessBoard[row][col] = 'Q';
-                // move to next row
-                nQueens(row + 1, chessBoard, ans);
-                // BACKTRACK
-                chessBoard[row][col] = '.';
+
+        for (int col = 0; col < board.length; col++) {
+            if (canPlace(row, col, board)) {
+                board[row][col] = 'Q';
+                nQueens(row + 1, board, ans);
+                board[row][col] = '.';
             }
         }
     }
 
-    static boolean canQueenBePlaced(int row, int col, char[][] chessBoard) {
-        // check column
+    private boolean canPlace(int row, int col, char[][] board) {
+
+        // column
         int i = row - 1;
         while (i >= 0) {
-            if (chessBoard[i][col] == 'Q') return false;
+            if (board[i][col] == 'Q')
+                return false;
             i--;
         }
 
-        // check left diagonal
+        // left diagonal
         i = row - 1;
         int j = col - 1;
         while (i >= 0 && j >= 0) {
-            if (chessBoard[i][j] == 'Q') return false;
+            if (board[i][j] == 'Q')
+                return false;
             i--;
             j--;
         }
 
-        // check right diagonal
+        // right diagonal
         i = row - 1;
         j = col + 1;
-
-        while (i >= 0 && j < chessBoard.length) {
-            if (chessBoard[i][j] == 'Q') return false;
+        while (i >= 0 && j < board.length) {
+            if (board[i][j] == 'Q')
+                return false;
             i--;
             j++;
         }
+
         return true;
     }
 }
