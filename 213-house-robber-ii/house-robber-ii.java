56076914019b39aeba01c805308ad4b6508757c2
Arpit Shukla <167569808+arpitshukla9@users.noreply.github.com>
2026-09-21 12:05:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (99.16%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,14 +1,11 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1) return nums[0];
+        if (n == 1) return nums[0]; 
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        int case1 = solve(nums, 0, n-1, dp);
 
-        // Scenario 1: Rob within range [0, n - 2]
-        int[] dp1 = new int[n];
-        Arrays.fill(dp1, -1);
-        int case1 = solve(nums, 0, n - 1, dp1);
-
-        // Scenario 2: Rob within range [1, n - 1]
         int[] dp2 = new int[n];
         Arrays.fill(dp2, -1);
         int case2 = solve(nums, 1, n, dp2);
@@ -16,13 +13,13 @@ class Solution {
         return Math.max(case1, case2);
     }
 
-    // Standard linear House Robber memoization helper
-    int solve(int[] arr, int i, int end, int[] dp) {
-        if (i >= end) return 0;
+    //  memoization
+    int solve(int[] arr, int i, int n, int[] dp) {
+        if (i >= n) return 0;
         if (dp[i] != -1) return dp[i];
 
-        int steal = arr[i] + solve(arr, i + 2, end, dp);
-        int skip = solve(arr, i + 1, end, dp);
+        int steal = arr[i] + solve(arr, i + 2, n, dp);
+        int skip = solve(arr, i + 1, n, dp);
 
         return dp[i] = Math.max(steal, skip);
     }
