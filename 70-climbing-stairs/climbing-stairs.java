a81cc791760727066b9bf66747624dcc7bb6b99d
Arpit Shukla <167569808+arpitshukla9@users.noreply.github.com>
2026-09-21 10:38:54 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (72.88%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,8 +1,10 @@
 class Solution {
     public int climbStairs(int n) {
         int[] memo = new int[n+1];
+        int[] tab = new int[n+1];
         Arrays.fill(memo, -1);
-        return solvebyMemo(n, memo);
+        // return solvebyMemo(n, memo);
+        return solvebyTab(n, tab);
     }
     // BRUTE-FORCE -> TLE
     int solve(int n ){
@@ -16,14 +18,21 @@ class Solution {
     int solvebyMemo(int n , int[] memo){
         if (n < 0) return 0;
         if (n == 0) return 1;
-
         // Return cached result if already computed
-        if (memo[n] != -1) {
-            return memo[n];
-        }
-
+        if (memo[n] != -1) return memo[n];
         // Store result in memo array and return
         memo[n] = solvebyMemo(n - 1, memo) + solvebyMemo(n - 2, memo);
         return memo[n];
+    }
+    // tabulation
+    int solvebyTab(int n , int[] tab){
+        if(n == 1 || n == 2 || n ==3) return n;
+        tab[0] = 0;
+        tab[1] = 1;
+        tab[2] = 2;
+        for(int i = 3; i <= n;i++){
+            tab[i] = tab[i-1] + tab[i-2];
+        }
+        return tab[n];
     }
 }
