925567476e96952b35d0f6c2ac938425fb1dcd3b
Arpit Shukla <167569808+arpitshukla9@users.noreply.github.com>
2026-09-23 19:16:14 +0530

    Time: 4 ms (80.62%) | Memory: 47.1 MB (14.49%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,28 +1,25 @@
 class Solution {
-    ListNode MergeTwoList(ListNode list1, ListNode list2) {
-        if(list1 == null) return list2;
-        if(list2 == null) return list1;
-
-        if(list1.val <= list2.val) {
-            list1.next = MergeTwoList(list1.next, list2);
-            return list1;
-        } else {
-            list2.next = MergeTwoList(list1, list2.next);
-            return list2;
-        }
-    }
-
-    ListNode PartitionAndMerge(int start, int end, ListNode[] list){
-        if(start > end) return null;
-        if(start == end) return list[start];
-        int mid = start + (end - start) / 2;
-        ListNode list1 = PartitionAndMerge(start, mid , list);
-        ListNode list2 = PartitionAndMerge(mid+1, end,  list);
-
-        return MergeTwoList(list1, list2);
-    }
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0 || lists == null) return null;
-        return PartitionAndMerge(0, lists.length - 1, lists);
+        if (lists == null || lists.length == 0) return null;
+
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a.val, b.val));
+        for (ListNode node : lists) {
+            if (node != null) {
+                minHeap.add(node);
+            }
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (!minHeap.isEmpty()) {
+            ListNode smallest = minHeap.poll();
+            current.next = smallest;
+            current = current.next;
+            if (smallest.next != null) {
+                minHeap.add(smallest.next);
+            }
+        }
+
+        return dummy.next;
     }
 }
