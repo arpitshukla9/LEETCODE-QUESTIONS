c7aabb2631f7754503ee92311351de48ae47aec2
Arpit Shukla <167569808+arpitshukla9@users.noreply.github.com>
2026-09-23 18:27:04 +0530

    Time: 11 ms (96.09%) | Memory: 47.4 MB (91.02%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -6,22 +6,28 @@ class Solution {
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
 
-        // 2. Min-Heap ordered by map values (frequencies)
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(
-                (a, b) -> Integer.compare(freqMap.get(a), freqMap.get(b)));
+        // 2. Bucket sort: array of lists where index = frequency
+        List<Integer>[] buckets = new List[nums.length + 1];
+        for (int key : freqMap.keySet()) {
+            int freq = freqMap.get(key);
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(key);
+        }
 
-        // 3. Maintain size K heap based on map keys
-        for (int num : freqMap.keySet()) {
-            minHeap.offer(num);
-            if (minHeap.size() > k) {
-                minHeap.poll(); // Evicts element with the lowest frequency
+        // 3. Gather top k elements starting from the highest frequency
+        int[] result = new int[k];
+        int index = 0;
+        for (int i = buckets.length - 1; i >= 0 && index < k; i--) {
+            if (buckets[i] != null) {
+                for (int num : buckets[i]) {
+                    result[index++] = num;
+                    if (index == k) break;
+                }
             }
         }
-        // 4. Extract results into an array
-        int[] result = new int[k];
-        for (int i = k - 1; i >= 0; i--) {
-            result[i] = minHeap.poll();
-        }
+
         return result;
     }
 }
