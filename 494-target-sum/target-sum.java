b3aaadc9cf3c183f88ebfe0350c0e203151e3004
Arpit Shukla <167569808+arpitshukla9@users.noreply.github.com>
2026-09-23 10:24:24 +0530

    Time: 4 ms (87.40%) | Memory: 42.8 MB (96.89%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,26 +1,22 @@
 class Solution {
-    static int sum;
-    int ways(int i, int[] arr, int res, int target , int[][] dp){
-        if(i == arr.length) {
-            if(res == target) return 1;
-            else return 0;
-        }
-        if(dp[i][res+sum] != -1) return dp[i][res+sum];
-        int add = ways(i+1, arr, res+arr[i], target, dp);
-        int sub = ways(i+1, arr, res-arr[i], target, dp);
-        return dp[i][res+sum] = add+sub;
-    }
     public int findTargetSumWays(int[] nums, int target) {
-        int n = nums.length;
-        sum = 0;
-        for(int ele : nums) sum += ele;
-        int[][] dp = new int[n][2*sum + 1];
-        int res = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < dp[0].length; j++){
-                dp[i][j] = -1;
+        int totalSum = 0;
+        for (int num : nums) totalSum += num;
+
+        // Base checks
+        if (Math.abs(target) > totalSum) return 0;
+        if ((target + totalSum) % 2 != 0 || (target + totalSum) < 0) return 0;
+
+        int s2 = (target + totalSum) / 2;
+        int[] dp = new int[s2 + 1];
+        dp[0] = 1; // Base case: 1 way to make sum 0 (empty subset)
+
+        for (int num : nums) {
+            for (int j = s2; j >= num; j--) {
+                dp[j] += dp[j - num];
             }
-        } 
-        return ways(0, nums, 0, target , dp);
+        }
+
+        return dp[s2];
     }
 }
